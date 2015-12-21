cbca0d60bff71680bf856264156cf0e684dab81f
chato94 <chato94@gmail.com>
2015-12-21 00:31:29 -0600

    New function containers for script.js readability.

---
+++ logo_logger.js
@@ -0,0 +1,91 @@
+// Prints the RSTB logo to the Chrome console in an awesome assortment of colors
+function logRSTBLogo () {
+      var UP = "background-color:rgb(84,84,84);";
+      console.log('%c        ' +
+                  '%c                                                ' +
+                  '%c        ', '', UP, '');
+      console.log('%c    ' +
+                  '%c    ' +
+                  '%c                                                ' +
+                  '%c    ' +
+                  '%c    ', '', UP, '', UP, '');
+      console.log('%c    ' +
+                  '%c                                                        ' +
+                  '%c    ', UP, '', UP);
+      console.log('%c    ' +
+                  '%c   ' +
+                  '%c            ' +
+                  '%c ' +
+                  '%c            ' +
+                  '%c ' +
+                  '%c            ' +
+                  '%c ' +
+                  '%c        ' +
+                  '%c      ' +
+                  '%c    ', UP, '', UP, '', UP, '', UP, '', UP, '', UP);
+      console.log('%c    ' +
+                  '%c   ' +
+                  '%c    ' +
+                  '%c    ' +
+                  '%c    ' +
+                  '%c ' +
+                  '%c    ' +
+                  '%c             ' +
+                  '%c    ' +
+                  '%c     ' +
+                  '%c    ' +
+                  '%c    ' +
+                  '%c    ' +
+                  '%c  ' +
+                  '%c    ', UP, '', UP, '', UP, '', UP, '', UP, '', UP, '', UP, '', UP);
+      console.log('%c    ' +
+                  '%c   ' +
+                  '%c        ' +
+                  '%c     ' +
+                  '%c            ' +
+                  '%c     ' +
+                  '%c    ' +
+                  '%c     ' +
+                  '%c        ' +
+                  '%c      ' +
+                  '%c    ', UP, '', UP, '', UP, '', UP, '', UP, '', UP);
+      console.log('%c    ' +
+                  '%c   ' +
+                  '%c    ' +
+                  '%c    ' +
+                  '%c    ' +
+                  '%c         ' +
+                  '%c    ' +
+                  '%c     ' +
+                  '%c    ' +
+                  '%c     ' +
+                  '%c    ' +
+                  '%c    ' +
+                  '%c    ' +
+                  '%c  ' +
+                  '%c    ', UP, '', UP, '', UP, '', UP, '', UP, '', UP, '', UP, '', UP);
+      console.log('%c    ' +
+                  '%c   ' +
+                  '%c    ' +
+                  '%c    ' +
+                  '%c    ' +
+                  '%c ' +
+                  '%c            ' +
+                  '%c     ' +
+                  '%c    ' +
+                  '%c     ' +
+                  '%c        ' +
+                  '%c      ' +
+                  '%c    ', UP, '', UP, '', UP, '', UP, '', UP, '', UP, '', UP);
+      console.log('%c    ' +
+                  '%c                                                        ' +
+                  '%c    ', UP, '', UP);
+      console.log('%c    ' +
+                  '%c    ' +
+                  '%c                                                ' +
+                  '%c    ' +
+                  '%c    ', '', UP, '', UP, '');
+      console.log('%c        ' +
+                  '%c                                                ' +
+                  '%c        ', '', UP, '');
+}
